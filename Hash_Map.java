1aba8954beb4ddd1314e1c7899d007844d08e625
Mohammad Aakil <aakilk735@gmail.com>
2025-01-27 20:26:15 +0530

    Add files via upload

---
+++ Hash_Map.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class Hash_Map {
+    public static void main(String[] args) {
+        HashMap<String,Integer>map=new HashMap<>();
+        map.put("Aakil",25);
+        map.put("Aman",18);
+        map.put("Anish",26);
+        map.put("Sohil",15);
+        System.out.println(map);
+    }
+}
